5459a815b5d80f011540c9b3c0abeafe9389ffcf
Özkan <ozkanadar@outlook.com>
2019-11-14 14:22:43 +0100

    initializing to github

---
+++ build/my.js
@@ -0,0 +1,8 @@
+
+$('.skill').on('click', 'button', function(){
+    var skillBar = $(this).siblings().find('.inner');
+    var skillVal = skillBar.attr("data-progress");
+    $(skillBar).animate({
+        height: skillVal
+    }, 2100);
+});
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+  return (
+
+    <div className="App">
+
+
+
+
+    <img src="qr.png" className="qrCode" width="150px" height="150px" alt="qr"/>
+    <img src="logo.png" className="logo" width="150px" height="150px" alt="logo"/>
+  <button className="btn" onclick="buttonClick5(),buttonClick6()">Donate 5€</button>
+
+
+
+    </div>
+  );
+}
+ }
+export default App;
